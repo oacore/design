ad393a963f0b26bcf373f7170c1bcb11e2595bc9
Jozef Harag <32jojo32@gmail.com>
2019-11-16 14:59:21 +0100

    Create icon component

---
+++ src/components/icon/index.jsx
@@ -0,0 +1,54 @@
+import React from 'react'
+import './index.scss'
+import PropTypes from 'prop-types'
+import DownloadIcon from './assets/download.svg'
+import OutlineIcon from './assets/outline.svg'
+import ThumbnailsIcon from './assets/thumbnails.svg'
+import PaperInfoIcon from './assets/paper_info.svg'
+import PrintIcon from './assets/print.svg'
+import ShareIcon from './assets/share.svg'
+import LeftArrowIcon from './assets/left-arrow.svg'
+import RightArrowIcon from './assets/right-arrow.svg'
+import RotateIcon from './assets/rotate.svg'
+import ZoomInIcon from './assets/zoom-in.svg'
+import ZoomOutIcon from './assets/zoom-out.svg'
+import CoreSymbolIcon from './assets/core-symbol.svg'
+
+const iconMap = {
+  'core-symbol': CoreSymbolIcon,
+  download: DownloadIcon,
+  outline: OutlineIcon,
+  thumbnails: ThumbnailsIcon,
+  'paper-info': PaperInfoIcon,
+  print: PrintIcon,
+  share: ShareIcon,
+  'left-arrow': LeftArrowIcon,
+  'right-arrow': RightArrowIcon,
+  'zoom-in': ZoomInIcon,
+  'zoom-out': ZoomOutIcon,
+  rotate: RotateIcon,
+}
+
+const mapNameToModule = name => {
+  if (!(name in iconMap)) throw new Error(`Icon ${name} not found`)
+  return iconMap[name]
+}
+
+const Icon = React.memo(({ iconType }) => {
+  const icon = mapNameToModule(iconType)
+  return (
+    <svg
+      className="oadesign-icon"
+      viewBox={`${icon.viewBox}`}
+    >
+      <use xlinkHref={`#${icon.id}`} />
+    </svg>
+  )
+})
+
+Icon.propTypes = {
+  /** The name of a icon */
+  iconType: PropTypes.oneOf(Object.keys(iconMap)).isRequired,
+}
+
+export default Icon
