c71fc53bae75b7376a540f0ac0b73e718a9c63a7
Viktor Yakubiv <viktor@yakubiv.com>
2019-11-20 16:25:57 +0200

    Setup PostCSS environment
    Sets up preset-env to add variable defaults.
    
    Adds possibility to have inline comments via
    SCSS syntax parser and strip-comments plugin.
    
    Add cleaning for final CSS file with CSSNano.

--- postcss.config.js
+++ postcss.config.js
@@ -1,15 +1,35 @@
 /* eslint-disable import/no-extraneous-dependencies, prettier/prettier */
 const presetEnv = require('postcss-preset-env')
+
+const calc = require('postcss-calc')
 const cssModules = require('postcss-modules')
+const stripInlineComments = require('postcss-strip-inline-comments')
+const nano = require('cssnano')
 
 const { cosmiconfigSync } = require('cosmiconfig')
 
 const cssModulesConfig = cosmiconfigSync('cssmodules').search().config
 
 module.exports = {
+  syntax: 'postcss-scss',
   map: true,
   plugins: [
-    presetEnv(),
+    stripInlineComments(),
+    calc(),
+    presetEnv({
+      importFrom: 'src/foundation/variables.css',
+    }),
+    nano({
+      preset: ['default', {
+        rawCache: false,
+        discardComments: false,
+        mergeLonghand: false,
+        normalizeWhitespace: false,
+        svgo: false,
+        reduceInitial: false,
+        reduceTransforms: false,
+      }]
+    }),
     cssModules({
       ...cssModulesConfig,
       getJSON: () => {}
