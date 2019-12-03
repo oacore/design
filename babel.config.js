aabeb3bae9c51b99415bdfca8c7a6d41e9f9813e
Viktor Yakubiv <viktor@yakubiv.com>
2019-12-03 15:39:29 +0200

    Add @babel/plugin-proposal-class-properties

--- babel.config.js
+++ babel.config.js
@@ -20,8 +20,9 @@ module.exports = {
   plugins: [
     '@babel/plugin-proposal-export-default-from',
     '@babel/plugin-proposal-export-namespace-from',
-    '@babel/plugin-transform-runtime',
+    '@babel/plugin-proposal-class-properties',
     '@babel/plugin-proposal-object-rest-spread',
+    '@babel/plugin-transform-runtime',
     ['css-modules-transform', cssModulesConfig],
   ],
 }
