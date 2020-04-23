a232a722128e9d9a07ced46a5cadb8ca370b5912
Jozef Harag <32jojo32@gmail.com>
2020-04-23 13:34:00 +0200

    Add Table Footer (#169)

--- src/elements/table/index.js
+++ src/elements/table/index.js
@@ -4,12 +4,14 @@ import HeadCell from './head-cell'
 import Body from './body'
 import Row from './row'
 import Cell from './cell'
+import Footer from './footer'
 
 Table.Head = Head
 Table.HeadCell = HeadCell
 Table.Body = Body
 Table.Row = Row
 Table.Cell = Cell
+Table.Footer = Footer
 
 export default Table
 export {
@@ -18,4 +20,5 @@ export {
   Body as TableBody,
   Row as TableRow,
   Cell as TableCell,
+  Footer as TableFooter,
 }
