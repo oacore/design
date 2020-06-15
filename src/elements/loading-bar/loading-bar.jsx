c2be089d0cde4992e7de4e6e4dc71272243eaa0d
Jozef Harag <32jojo32@gmail.com>
2020-06-15 15:16:02 +0200

    Add Loading Bar (#190)

---
+++ src/elements/loading-bar/loading-bar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import { classNames } from '../../utils'
+import styles from './loading-bar.css'
+
+const LoadingBar = ({ className, fixed = false, tag: Tag = 'div' }) => (
+  <Tag
+    className={classNames
+      .use(styles.loadingBar, fixed && styles.fixed)
+      .join(className)}
+  />
+)
+
+export default LoadingBar
---
+++ src/elements/loading-bar/loading-bar.md
@@ -0,0 +1,3 @@
+```jsx
+<LoadingBar isLoading/>
+```
