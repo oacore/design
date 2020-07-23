d5bedeaf538b8632ed4a994bd4cbbd4e1aab6521
Jozef Harag <32jojo32@gmail.com>
2020-07-23 13:15:45 +0200

    Move forwardRef closer to Component
    Fixes:
    Warning: forwardRef render functions do not support propTypes or defaultProps. Did you accidentally pass a React component?

--- src/elements/progress-spinner/progress-spinner.jsx
+++ src/elements/progress-spinner/progress-spinner.jsx
@@ -13,29 +13,28 @@ const progressValues = (value, max, { radius = 9 } = {}) => {
   return [filled, empty].join(' ')
 }
 
-const ProgressSpinner = (
-  { children, className, value, max = 1.0, ...htmlProps },
-  ref
-) => (
-  <svg
-    ref={ref}
-    className={classNames
-      .use(styles.spinner, value == null && styles.indeterminate)
-      .join(className)}
-    viewBox="0 0 24 24"
-    xmlns="http://www.w3.org/2000/svg"
-    {...htmlProps}
-  >
-    <circle
-      strokeWidth="2"
-      strokeLinecap="butt"
-      strokeDasharray={progressValues(value, max)}
-      cx="12"
-      cy="12"
-      r="9"
-    />
-    {children}
-  </svg>
+const ProgressSpinner = forwardRef(
+  ({ children, className, value, max = 1.0, ...htmlProps }, ref) => (
+    <svg
+      ref={ref}
+      className={classNames
+        .use(styles.spinner, value == null && styles.indeterminate)
+        .join(className)}
+      viewBox="0 0 24 24"
+      xmlns="http://www.w3.org/2000/svg"
+      {...htmlProps}
+    >
+      <circle
+        strokeWidth="2"
+        strokeLinecap="butt"
+        strokeDasharray={progressValues(value, max)}
+        cx="12"
+        cy="12"
+        r="9"
+      />
+      {children}
+    </svg>
+  )
 )
 
-export default forwardRef(ProgressSpinner)
+export default ProgressSpinner
