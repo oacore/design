a43e0f67c80f1957e52a8555cec8d1653ec50163
Jozef Harag <32jojo32@gmail.com>
2020-07-23 12:07:44 +0200

    Add missing forwardRef to FormControl

--- src/elements/forms/control.jsx
+++ src/elements/forms/control.jsx
@@ -4,29 +4,26 @@ import styles from './styles.css'
 
 import { classNames } from 'utils'
 
-const FormControl = ({
-  type: controlType,
-  children,
-  className,
-  focus,
-  ...htmlProps
-}) => {
-  const Tag = ['select', 'textarea'].includes(controlType)
-    ? controlType
-    : 'input'
-  const type = Tag === 'input' ? controlType : undefined
+const FormControl = React.forwardRef(
+  ({ type: controlType, children, className, focus, ...htmlProps }, ref) => {
+    const Tag = ['select', 'textarea'].includes(controlType)
+      ? controlType
+      : 'input'
+    const type = Tag === 'input' ? controlType : undefined
 
-  return (
-    <Tag
-      type={type}
-      className={classNames
-        .use(styles.control, focus && styles.focus)
-        .join(className)}
-      {...htmlProps}
-    >
-      {children}
-    </Tag>
-  )
-}
+    return (
+      <Tag
+        ref={ref}
+        type={type}
+        className={classNames
+          .use(styles.control, focus && styles.focus)
+          .join(className)}
+        {...htmlProps}
+      >
+        {children}
+      </Tag>
+    )
+  }
+)
 
 export default FormControl
