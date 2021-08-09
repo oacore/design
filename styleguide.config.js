eab3df7227a5e86519a289b90dd6ef149c81978f
Kostiantyn Voskoboinik <50716463+vkot91@users.noreply.github.com>
2021-08-09 12:06:22 +0300

    Create carousel component (#452)
    * Create carousel component
    
    * Create markdown for carousel
    
    * Update carousel markdown file
    
    * Update carousel documentation
    
    * Add svg-sprite-loade plugin

--- styleguide.config.js
+++ styleguide.config.js
@@ -1,5 +1,7 @@
 const path = require('path')
 
+const SpriteLoaderPlugin = require('svg-sprite-loader/plugin')
+
 module.exports = {
   title: 'CORE Design Elements',
   usageMode: 'expand',
@@ -147,8 +149,31 @@ module.exports = {
             'svgo-loader',
           ],
         },
+        {
+          test: /\.(png|jpg|gif)$/i,
+          use: [
+            {
+              loader: 'url-loader',
+              options: {
+                limit: 8192,
+              },
+            },
+          ],
+        },
+        {
+          test: /\.(woff(2)?|ttf|eot)(\?v=\d+\.\d+\.\d+)?$/,
+          use: [
+            {
+              loader: 'file-loader',
+              options: {
+                name: '[name].[ext]',
+              },
+            },
+          ],
+        },
       ],
     },
+    plugins: [new SpriteLoaderPlugin()],
     resolve: {
       extensions: ['.js', '.jsx'],
     },
