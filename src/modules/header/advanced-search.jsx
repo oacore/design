bb4989a5e8f7da4b4b1e25ec3dd77f74fdddf490
Kostiantyn Voskoboinik <50716463+vkot91@users.noreply.github.com>
2021-11-17 12:28:34 +0200

    Make fieldset component (#471)
    * Create fieldset component
    
    * Update packages version
    
    * Remove unused package

--- src/modules/header/advanced-search.jsx
+++ src/modules/header/advanced-search.jsx
@@ -1,7 +1,8 @@
 import React, { memo } from 'react'
 
 import styles from './advanced-search.css'
-import { Button, Icon } from '../../elements'
+
+import { Button, Icon } from 'elements'
 
 const ACTION_BUTTONS_VALUES = ['doi', 'title', 'oai', 'issn ']
 
