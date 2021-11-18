dcd6c2b56374d4876f22236d051e8e71a9db1cd5
Kostiantyn Voskoboinik <50716463+vkot91@users.noreply.github.com>
2021-11-18 12:30:34 +0200

    Update select (#479)
    * Minor update select component
    
    * Delete extra variable usage

--- src/modules/select/select.jsx
+++ src/modules/select/select.jsx
@@ -1,6 +1,5 @@
-import React, { useEffect, useState, useCallback, memo } from 'react'
+import React, { useEffect, useState, useCallback, useRef, memo } from 'react'
 import PropTypes from 'prop-types'
-import { useRef } from 'react/cjs/react.development'
 
 import { useInput, useOptions } from './hooks'
 import styles from './select.css'
