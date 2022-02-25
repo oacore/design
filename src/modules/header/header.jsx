a44985aa4709a4763d26edba7833c2f16ce19938
mcancellieri <mcancellieri@users.noreply.github.com>
2022-02-25 12:43:19 +0000

    Adds message to support UA (#520)
    * Adds message support UA

--- src/modules/header/header.jsx
+++ src/modules/header/header.jsx
@@ -126,6 +126,13 @@ const Header = ({ children, className, id, ...passProps }) => {
       <AppBar.Item className={styles.logoContainer}>
         <a href="/" className={styles.logoLink}>
           <Logo className={styles.logo} />
+          &nbsp;
+          <p className={styles.peace}>
+            <span role="img" aria-label="Ukranian Flag">
+              🇺🇦&nbsp;
+            </span>
+            &nbsp; make better metadata, not war
+          </p>
         </a>
       </AppBar.Item>
 
