0a1c867ba8faf40ebf5db0f2cb12ac1ed74b1775
Kostiantyn Voskoboinik <50716463+vkot91@users.noreply.github.com>
2022-05-06 11:18:59 +0300

    Add possibility to refresh default value (#538)

--- src/hooks/use-switch.js
+++ src/hooks/use-switch.js
@@ -1,7 +1,11 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
 const useSwitch = (defaultChecked = false) => {
   const [isActivated, setIsActivated] = React.useState(defaultChecked)
+
+  useEffect(() => {
+    setIsActivated(defaultChecked)
+  }, [defaultChecked])
 
   const toggleChange = () => {
     setIsActivated(!isActivated)
