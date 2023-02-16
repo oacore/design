a5d2a71a34d01276334ae923d2b83f06ccdbfadd
ekachxaidze98 <65679299+ekachxaidze98@users.noreply.github.com>
2023-02-16 17:42:58 +0400

    CORE-4461: add active state on redirect (#601)
    * CORE-4461: add active state on redirect
    
    * Replace img
    
    ---------
    
    Co-authored-by: admin <valerij.budko@gmail.com>

--- src/modules/documentation-membership-nav/documentation-membership-nav.jsx
+++ src/modules/documentation-membership-nav/documentation-membership-nav.jsx
@@ -2,14 +2,19 @@ import React, { useState } from 'react'
 
 import styles from './styles.css'
 import { classNames } from '../../utils'
-import activeArrow from '../../../public/icons/active-arrow.svg'
 
-const DocumentationMembershipNav = ({ textData, setHighlight }) => {
+const DocumentationMembershipNav = ({
+  textData,
+  setHighlight,
+  activeIndex,
+  setNavActiveIndex,
+}) => {
   const [activeItem, setActiveItem] = useState(null)
 
   const handleClick = (obj, item) => {
     setActiveItem(item)
     setHighlight(+item)
+    setNavActiveIndex(null)
     const element = document.getElementById(obj.href.replace('#', ''))
     if (element) {
       element?.scrollIntoView({
@@ -21,20 +26,23 @@ const DocumentationMembershipNav = ({ textData, setHighlight }) => {
 
   return (
     <ul className={styles.sider}>
-      {Object.keys(textData.navItems).map((item) => (
+      {Object.keys(textData.navItems).map((item, i) => (
         // eslint-disable-next-line max-len
         // eslint-disable-next-line jsx-a11y/no-noninteractive-element-interactions,jsx-a11y/click-events-have-key-events
         <li
           className={classNames.use(styles.siderItem, {
-            [styles.activeItem]: activeItem === item,
+            [styles.activeItem]: !activeIndex
+              ? activeItem === item
+              : activeIndex === i,
           })}
           key={textData.navItems[item].item}
           onClick={() => handleClick(textData.navItems[item], item)}
         >
           {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
           <a className={styles.siderItemLink}>{textData.navItems[item].item}</a>
-          {activeItem === item ? (
-            <img src={activeArrow} alt="Logo" className={styles.logo} />
+          {activeItem === item || activeIndex === i ? (
+            // eslint-disable-next-line react/no-unescaped-entities
+            <span className={styles.logo}>></span>
           ) : (
             ''
           )}
