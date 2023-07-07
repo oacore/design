e21288c4b962522d5a031075d578aba538231ef1
ekachxaidze98 <65679299+ekachxaidze98@users.noreply.github.com>
2023-07-07 16:53:13 +0400

    add logo size lg (#629)

--- src/elements/logo/data-provider.jsx
+++ src/elements/logo/data-provider.jsx
@@ -32,7 +32,7 @@ const DataProviderLogo = ({
 DataProviderLogo.propTypes = {
   alt: PropTypes.string.isRequired,
   imageSrc: PropTypes.string,
-  size: PropTypes.oneOf(['md', 'sm', 'ml', 'lg']),
+  size: PropTypes.oneOf(['md', 'sm', 'lg']),
   /* Pass true if you want to use default icon */
   useDefault: PropTypes.bool,
 }
