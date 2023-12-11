804c4bd049a35ab2f7b770512ea1c9822022fac2
ekachxaidze98 <65679299+ekachxaidze98@users.noreply.github.com>
2023-12-11 17:33:10 +0400

    CORE: reuse docs (#651)

--- src/modules/documentation-membership/documentation-membership.jsx
+++ src/modules/documentation-membership/documentation-membership.jsx
@@ -4,76 +4,115 @@ import ReactMarkdown from 'react-markdown'
 import styles from './styles.css'
 import { classNames } from '../../utils'
 
+const ASSETS_BASE_URL = 'https://oacore.github.io/content/'
+
 const DocumentationMembership = ({
   headerTitle,
   headerCaption,
   highlight,
   nav,
   docs,
+  imageSource,
 }) => (
   <div className={styles.documentationWrapper}>
     <h2 className={styles.documentationHeader}>{headerTitle}</h2>
     {headerCaption}
-    <div className={styles.placement}>
+    <div
+      className={classNames.use(styles.placement, {
+        [styles.placementHeight]: imageSource,
+      })}
+    >
       {nav}
       <div className={styles.documentationInnerWrapper}>
         {docs?.map((item, index) => (
-          <div key={item.id} className={styles.documentationItem} id={item.id}>
-            <h3
-              className={classNames.use(styles.documentationItemTitle, {
-                [styles.highlighted]: highlight === index,
-              })}
+          <>
+            {item.divider && <div className={styles.divider} />}
+            <div
+              key={item.id}
+              className={styles.documentationItem}
+              id={item.id}
             >
-              {item.title}
-            </h3>
-            <div className={styles.typeWrapper}>
-              {Object.entries(item?.membershipTypes).map((value) => (
-                <span
-                  key={item.id + value[0] + value[1]}
-                  className={classNames
-                    .use(styles.membership)
-                    .join(
-                      value[1] === 'Yes' ? styles.enabled : styles.disabled
-                    )}
-                >
-                  {value[0]}
-                </span>
-              ))}
-            </div>
-            <ReactMarkdown>{item.descriptionAbout}</ReactMarkdown>
-            <div>
-              {item?.images?.map((img, i) => (
-                <div className={styles.cardWrapper}>
-                  <div
-                    className={classNames.use({
-                      [styles.imgWrapper]: img.source,
-                    })}
-                  >
-                    {/* eslint-disable-next-line jsx-a11y/img-redundant-alt */}
-                    <img
-                      className={classNames.use(styles.image, {
-                        [styles.logoBanner]: item.id === 'logo-banner',
-                        [styles.logoPersonalised]:
-                          item.id === 'personalised-banner',
-                        [styles.badgeImage]: img.source,
-                        [styles.badgeImageHeight]:
-                          img.source?.includes('square'),
-                      })}
-                      src={img.file}
-                      alt="image"
-                      // eslint-disable-next-line react/no-array-index-key
-                      key={item.id + i}
-                    />
-                  </div>
-                  {img.source && (
-                    <div className={styles.textAlignment}>
-                      <span className={styles.text}>{img.source}</span>
-                    </div>
-                  )}
+              <h3
+                className={classNames.use(styles.documentationItemTitle, {
+                  [styles.highlighted]: highlight === index,
+                })}
+              >
+                {item.title}
+              </h3>
+              {item.membershipTypes && (
+                <div className={styles.typeWrapper}>
+                  {Object.entries(item?.membershipTypes).map((value) => (
+                    <span
+                      key={item.id + value[0] + value[1]}
+                      className={classNames
+                        .use(styles.membership)
+                        .join(
+                          value[1] === 'Yes' ? styles.enabled : styles.disabled
+                        )}
+                    >
+                      {value[0]}
+                    </span>
+                  ))}
                 </div>
-              ))}
+              )}
+              <ReactMarkdown linkTarget="_blank" className={styles.test}>
+                {item.descriptionAbout}
+              </ReactMarkdown>
+              <div>
+                {item?.images?.map((img, i) => (
+                  <div className={styles.cardWrapper}>
+                    <div
+                      className={classNames.use({
+                        [styles.imgWrapper]: img.source,
+                        [styles.imgSpread]: imageSource,
+                      })}
+                    >
+                      {/* eslint-disable-next-line max-len */}
+                      {/* eslint-disable-next-line jsx-a11y/img-redundant-alt */}
+                      <img
+                        className={classNames.use(styles.image, {
+                          [styles.logoBanner]: item.id === 'logo-banner',
+                          [styles.logoPersonalised]:
+                            item.id === 'personalised-banner',
+                          [styles.badgeImage]: img.source,
+                          [styles.badgeImageHeight]:
+                            img.source?.includes('square'),
+                        })}
+                        src={
+                          imageSource ? ASSETS_BASE_URL + img.file : img.file
+                        }
+                        alt="image"
+                        // eslint-disable-next-line react/no-array-index-key
+                        key={item.id + i}
+                      />
+                    </div>
+                    {img.source && (
+                      <div className={styles.textAlignment}>
+                        <span className={styles.text}>{img.source}</span>
+                      </div>
+                    )}
+                  </div>
+                ))}
+              </div>
+              {item?.subTitle && (
+                <ReactMarkdown className={styles.documentationSubTitle}>
+                  {item?.subTitle}
+                </ReactMarkdown>
+              )}
+              {item?.subDescription && (
+                <ReactMarkdown
+                  className={classNames.use(
+                    styles.documentationSubDescription,
+                    {
+                      [styles.subBorder]: item.border,
+                    }
+                  )}
+                >
+                  {item?.subDescription}
+                </ReactMarkdown>
+              )}
             </div>
-          </div>
+          </>
         ))}
       </div>
     </div>
