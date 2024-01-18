44abeb8fe7fc5ee694ed19f37761e828e8e673d9
Eka <ekachxaidze2@gmail.com>
2024-01-18 12:31:52 +0400

    CORE-4784: docs update style

--- src/modules/documentation-membership-nav/documentation-membership-nav.jsx
+++ src/modules/documentation-membership-nav/documentation-membership-nav.jsx
@@ -10,6 +10,7 @@ const DocumentationMembershipNav = ({
   setHighlight,
   activeIndex,
   setNavActiveIndex,
+  mulltyDocs,
 }) => {
   const [activeItem, setActiveItem] = useState(null)
   const headerHeight = 56
@@ -32,35 +33,42 @@ const DocumentationMembershipNav = ({
   }
 
   return (
-    <ul className={styles.sider}>
-      {Object.values(textData.navItems).map((item, i) => (
-        // eslint-disable-next-line max-len
-        // eslint-disable-next-line jsx-a11y/no-noninteractive-element-interactions,jsx-a11y/click-events-have-key-events
-        <li
-          className={classNames.use(styles.siderItem, {
-            [styles.hiddenItem]: item.hidden,
-            [styles.activeItem]: !activeIndex
-              ? activeItem === i
-              : activeIndex === i,
-          })}
-          key={item.item}
-          onClick={() => handleClick(item, i)}
-        >
-          {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
-          <ReactMarkdown className={styles.siderItemLink}>
-            {item.item}
-          </ReactMarkdown>
-          {activeItem === i || activeIndex === i ? (
-            // eslint-disable-next-line react/no-unescaped-entities
-            <span className={styles.logo}>
-              <Icon src="#active-arrow" />
-            </span>
-          ) : (
-            ''
-          )}
-        </li>
-      ))}
-    </ul>
+    <div
+      className={classNames.use({
+        [styles.siderWrapper]: mulltyDocs,
+      })}
+    >
+      {mulltyDocs && <div className={styles.placeholder}>Outline</div>}
+      <ul className={styles.sider}>
+        {Object.values(textData.navItems).map((item, i) => (
+          // eslint-disable-next-line max-len
+          // eslint-disable-next-line jsx-a11y/no-noninteractive-element-interactions,jsx-a11y/click-events-have-key-events
+          <li
+            className={classNames.use(styles.siderItem, {
+              [styles.hiddenItem]: item.hidden,
+              [styles.activeItem]: !activeIndex
+                ? activeItem === i
+                : activeIndex === i,
+            })}
+            key={item.item}
+            onClick={() => handleClick(item, i)}
+          >
+            {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
+            <ReactMarkdown className={styles.siderItemLink}>
+              {item.item}
+            </ReactMarkdown>
+            {activeItem === i || activeIndex === i ? (
+              // eslint-disable-next-line react/no-unescaped-entities
+              <span className={styles.logo}>
+                <Icon src="#active-arrow" />
+              </span>
+            ) : (
+              ''
+            )}
+          </li>
+        ))}
+      </ul>
+    </div>
   )
 }
 
