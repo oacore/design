ec4fdb5fb61c4da42076bb94c0bbb5e7a8465536
ekachxaidze98 <65679299+ekachxaidze98@users.noreply.github.com>
2025-10-07 18:11:55 +0400

    CORE: add video comp (#783)

---
+++ src/elements/video/index.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+import styles from './styles.css'
+import { Modal } from '../../modules'
+
+const Video = ({ video, visibleModal, closeModal }) =>
+  visibleModal && (
+    <Modal
+      onClose={closeModal}
+      aria-label="Service video"
+      className={styles.modal}
+    >
+      <iframe
+        className={styles.video}
+        src={video?.src}
+        title={video?.title}
+        allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+        allowFullScreen
+      />
+    </Modal>
+  )
+
+export default Video
