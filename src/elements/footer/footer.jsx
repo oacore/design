9d0cfb529c4c133d52bbd6c77a00866f68574bca
ekachxaidze98 <ekachxaidze2@gmail.com>
2026-09-11 14:20:50 +0400

    CORE: fix lint

--- src/elements/footer/footer.jsx
+++ src/elements/footer/footer.jsx
@@ -2,12 +2,7 @@ import React from 'react'
 
 import styles from './footer.css'
 // eslint-disable-next-line import/no-cycle
-import {
-  Logo as LogoFoot,
-  Icon as IconFoot,
-  Link as LinkFoot,
-  MathMarkdown,
-} from '..'
+import { Icon as IconFoot, Link as LinkFoot, MathMarkdown } from '..'
 import { classNames } from '../../utils'
 import { BaseLink as Link } from '../link'
 
